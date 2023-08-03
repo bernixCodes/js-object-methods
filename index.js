b6ce4373c7a0d93453b2441d195e3eb24e62d8d9
bernixCodes <adjoa.bernie@gmail.com>
2023-08-03 10:58:48 +0000

    refractor

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
 console.log("Testing my understanding of data types.");
+console.log("\n");
 
 //create an object
 let studentObj = {
   name: "bernice",
-  email: "zbernyx@gmail.com",
+  email: "zbn@gmail.com",
   course: "software engineering",
 };
 
 let studentArr = Object.entries(studentObj);
-
+console.log("student Arr");
 console.log(studentArr);
-console.log("High Level Sorted Student Arr:", studentArr.sort());
+console.log(studentArr.sort());
+console.log("\n");
 
 let studentArr_values = Object.values(studentObj);
-console.log("student object values:", studentArr_values);
-console.log("sorted student object values:", studentArr_values.sort());
+console.log("student object values:");
+console.log(studentArr_values);
+console.log("sorted:", studentArr_values.sort());
+console.log("\n");
 
 let studentArr_keys = Object.keys(studentObj);
-console.log("student object keys:", studentArr_keys);
-console.log("sorted student object keys:", studentArr_keys.sort());
+console.log("student object keys:");
+console.log(studentArr_keys);
+console.log("sorted:", studentArr_keys.sort());
+console.log("\n");
 
 let key_value_arr = [...studentArr_keys, ...studentArr_values];
-console.log("Key and values together: ", key_value_arr);
+console.log("Key and values together: ");
+console.log(key_value_arr);
 console.log(key_value_arr.sort());
